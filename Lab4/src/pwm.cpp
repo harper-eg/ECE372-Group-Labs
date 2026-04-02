4ecb1fcf0e5907b16e2bc770dafb3c98aae6eadc
eliasathomas <111325454+Rasberrypi101@users.noreply.github.com>
2026-04-02 15:15:29 -0700

    Implemented duty cyle

--- Lab4/src/pwm.cpp
+++ Lab4/src/pwm.cpp
@@ -41,11 +41,29 @@ OCR3A =  255;
  * The adcValue is the combination of ADCH and ADCL (0-1023).
  */
 void changeDutyCycle(unsigned int adcValue){
+    int dutyCycle;
     // TODO: Map adcValue to duty cycle and direction
+    if (adcValue < 127) {
+        dutyCycle = (adcValue - 127) * 255 * 2 / 1023; // Map 0-1023 to 0-255
+        PORTB |= (1 << PORTB0); // Clockwise: PB0 = HIGH, PB1 = LOW
+        PORTB &= ~(1 << PORTB1);
+    }
+    else if (adcValue == 127) {
+        dutyCycle = 0; // Stop motor at midpoint
+        PORTB &= ~((1 << PORTB0) | (1 << PORTB1)); // Stop: PB0 = LOW, PB1 = LOW
+    }
+    else {
+        dutyCycle = adcValue * 255 * 2 / 1023; // Map 0-1023 to 0-255
+        PORTB |= (1 << PORTB1); // Counterclockwise: PB0 = LOW, PB1 = HIGH
+        PORTB &= ~(1 << PORTB0);
+    }
+
     // D = OCR3A/ TOP
     // TODO: Set OCR3A (and optionally OCR4A) to control speed
+    OCR3A = dutyCycle;
     // TODO: Control direction via PB0 (IN1) and PB1 (IN2) on L293D:
     //       Clockwise:          PB0 = HIGH, PB1 = LOW
     //       Counterclockwise:   PB0 = LOW,  PB1 = HIGH
     //       Stop:               PB0 = LOW,  PB1 = LOW
+
 }
